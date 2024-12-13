84702c93f5b65bdb4e856072df1179353fc2faa5
yeonnhuu <yeonnhuu@gmail.com>
2024-12-13 20:07:32 +0900

    :recycle: Refactor(Constants): 상수 리팩토링

--- src/main/java/oncall/util/Constants.java
+++ src/main/java/oncall/util/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
 
     public static final String ERROR_PREFIX = "[ERROR] ";
     public static final String INPUT_DELIMITER = ",";
-
     public static final int PLAN_START_SIZE = 2;
     public static final int STAFFS_MIN_NUMBER = 5;
     public static final int STAFFS_MAX_NUMBER = 35;
