f1bca903095be36684b785deeeb943587a9d5f58
yeonnhuu <yeonnhuu@gmail.com>
2024-12-13 19:29:15 +0900

    :sparkles: feat(OutputView): 출력 기능 추가

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public void printErrorMessage(String message) {
         System.out.println(message);
     }
+
+    public void printPlanCalendar(String planCalendar) {
+        System.out.println(planCalendar);
+    }
 }
