87276c1cbef3d24323b68e98a9d3814859e3b3af
baaamk <brandon55321@gmail.com>
2026-01-02 00:23:17 +0900

    feat: 근로자 vo 와 검증 기능 구현

---
+++ src/main/java/oncall/domain/Employee.java
@@ -0,0 +1,32 @@
+package oncall.domain;
+
+import oncall.exception.ErrorMessage;
+
+import java.util.regex.Pattern;
+
+public class Employee {
+    private static final Pattern REGEXP_KOR = Pattern.compile("^[가-힣]*$");
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+
+    private Employee(String name) {
+        this.name = name;
+    }
+
+    public static Employee from(String name) {
+        validateName(name);
+        return new Employee(name);
+    }
+
+    private static void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE_NAME.getMessage());
+        }
+
+        if (!REGEXP_KOR.matcher(name).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FORMAT_NAME.getMessage());
+        }
+
+    }
+}
