ab5984ac09eef3c238e2fec3be0e794a55999049
baaamk <brandon55321@gmail.com>
2026-01-02 01:32:19 +0900

    feat: 휴일 추가 구현

--- src/main/java/oncall/domain/vo/Month.java
+++ src/main/java/oncall/domain/vo/Month.java
@@ -1,25 +1,32 @@
 package oncall.domain.vo;
 
+import java.util.Set;
+
 public enum Month {
-    JANUARY(1, 31),
-    FEBRUARY(2, 28),
-    MARCH(3, 31),
-    APRIL(4, 30),
-    MAY(5, 31),
-    JUNE(6, 30),
-    JULY(7, 31),
-    AUGUST(8, 31),
-    SEPTEMBER(9, 30),
-    OCTOBER(10, 31),
-    NOVEMBER(11, 30),
-    DECEMBER(12, 31);
+    JANUARY(1, 31, Set.of(1)),
+    FEBRUARY(2, 28, Set.of()),
+    MARCH(3, 31, Set.of(1)),
+    APRIL(4, 30, Set.of()),
+    MAY(5, 31, Set.of(5)),
+    JUNE(6, 30, Set.of(6)),
+    JULY(7, 31, Set.of()),
+    AUGUST(8, 31, Set.of(15)),
+    SEPTEMBER(9, 30, Set.of()),
+    OCTOBER(10, 31, Set.of(3, 9)),
+    NOVEMBER(11, 30, Set.of()),
+    DECEMBER(12, 31, Set.of(25));
 
     private final int month;
     private final int days;
 
-    Month(int month, int days) {
+
+
+    private final Set<Integer> holidays;
+
+    Month(int month, int days, Set<Integer> holidays) {
         this.month = month;
         this.days = days;
+        this.holidays = holidays;
     }
 
     public int getMonth() {
@@ -28,5 +35,9 @@ public enum Month {
 
     public int getDays() {
         return days;
+    }
+
+    public Set<Integer> getHolidays() {
+        return holidays;
     }
 }
